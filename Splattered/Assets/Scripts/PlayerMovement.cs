0ff04e35eaa2de276cfacc5062b7468446f54805
KingPabloIV <40045413+KingPabloIV@users.noreply.github.com>
2023-04-20 19:01:54 -0700

    added jumping

--- Splattered/Assets/Scripts/PlayerMovement.cs
+++ Splattered/Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck; //position of ground check
     [SerializeField] private Vector2 groundCheckSize; //radius of ground check
     public float lastOnGround; //last time player was on ground
+    [SerializeField] private bool isGrounded; //is player on ground
     #endregion
+    public bool testGround;
 
     // Start is called before the first frame update
     void Start()
@@ -31,16 +33,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lastOnGround -= (lastOnGround > -0.1) ? Time.deltaTime: 0;
+        lastOnGround -= (lastOnGround > -0.1) ? Time.deltaTime : 0;
         moveInput.x = Input.GetAxisRaw("Horizontal");
-        if(Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, LayerMask.GetMask("Ground")))
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        if (Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, LayerMask.GetMask("Ground")))
             lastOnGround = 0.1f;
+        if (lastOnGround > 0)
+            isGrounded = true;
+        else
+            isGrounded = false;
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            Jump();
+            Debug.Log("jumping");
+        }
 
     }
 
     void FixedUpdate()
     {
         Run();
+        
     }
 
     private void Run()
@@ -49,7 +61,7 @@ public class PlayerMovement : MonoBehaviour
 
         #region Caluclate Acceleration
         float accelRate;
-        if(lastOnGround > 0)
+        if (isGrounded)
             accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Data.runAccelAmount : Data.runDeccelAmount;
         else
             accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Data.runAccelAmount * Data.accelInAir : Data.runDeccelAmount * Data.deccelInAir;
@@ -63,12 +75,23 @@ public class PlayerMovement : MonoBehaviour
         #endregion
 
         #region Friction
-        if(lastOnGround > 0 && Mathf.Abs(moveInput.x) < 0.01f){
+        if (lastOnGround > 0 && Mathf.Abs(moveInput.x) < 0.01f)
+        {
             float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), Data.runDeccelAmount * Time.deltaTime);
             amount *= Mathf.Sign(rb.velocity.x);
             rb.AddForce(-amount * Vector2.right, ForceMode2D.Force);
         }
-            
+
         #endregion
+    }
+
+    private void Jump()
+    {
+        lastOnGround = 0;
+
+        float force = Data.jumpForce;
+        if(rb.velocity.y < 0)
+            force -= rb.velocity.y;
+        rb.AddForce(force * Vector2.up, ForceMode2D.Impulse);
     }
 }
--- Splattered/Assets/Scripts/PlayerMovementData.cs
+++ Splattered/Assets/Scripts/PlayerMovementData.cs
@@ -11,17 +11,40 @@ public class PlayerMovementData : MonoBehaviour
     public float decelerationTime; //time it should take for player to decelerate from max speed to 0
     [HideInInspector] public float runDeccelAmount; //force applied to player to decelerate
 
-    [Space(10)]
+    [Space(4)]
     [Range(0.01f, 1)] public float accelInAir; //Multipliers applied to acceleration rate when airborne.
     [Range(0.01f, 1)] public float deccelInAir;
+    
+    [Space(10)]
+    [Header("Jump")]
+    public float jumpHeight;
+    public float jumpToApexTime;
+    [HideInInspector]public float jumpForce;
+
+    [Header("Gravity")]
+    [HideInInspector] public float gravityStrength;
+    [HideInInspector] public float gravityScale;
+    public float fallGravityMultiplier;
+    public float maxFallSpeed;
+
+
 
     // Runs when a value is changed in the inspector
     private void OnValidate()
     {
-        #region calculaitons
+        #region Run Calculations
         // Calculate acceleration and deceleration amounts
         runAccelAmount = (50f * accelerationTime) / maxSpeed;
         runDeccelAmount = maxSpeed / decelerationTime;
+        #endregion
+
+        #region Gravity Calculations
+        gravityStrength = -(2 * jumpHeight) / (jumpToApexTime * jumpToApexTime);
+        gravityScale = gravityStrength / Physics2D.gravity.y;
+        #endregion
+
+        #region Jump Calculations
+        jumpForce = Mathf.Abs(gravityStrength) * jumpToApexTime;
         #endregion
     }
 }
