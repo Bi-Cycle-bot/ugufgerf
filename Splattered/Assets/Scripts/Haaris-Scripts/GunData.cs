eb1f9e7af1ff44421dd0ccdd3f8830196f96ac18
Shadragon99 <haaris2001@gmail.com>
2023-05-07 16:38:22 -0700

    Updated .gitignore and added Music

---
+++ Splattered/Assets/Scripts/Haaris-Scripts/GunData.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GunData : MonoBehaviour
+{
+    #region Gun Stats
+    [Header("Gun Stats")]
+    public float fireRate;
+    public float damage;
+    public float range;
+    public float reloadTime;
+    public float bulletSpeed;
+    public int springMag, springsPerTap;
+    public bool isHitscan;
+    //public int numberOfBullets;
+    public float spread;
+    public bool knockback;
+    public bool allowButtonHold;
+    public int springsLeft, springsUsed;
+    #endregion
+    [HideInInspector] public bool shooting, reloading;
+    [HideInInspector] public float bulletLifeTime;
+    [HideInInspector] public float timeBetweenShots;
+    [HideInInspector] public float timeBetweenShooting;
+
+    //All references
+    public Transform attackPoint;
+    public RaycastHit rayHit;
+    public LayerMask whatIsEnemy;
+    
+    //Graphics
+    public GameObject muzzleFlash;
+    public TextMeshProUGUI text;
+    
+
+    void OnValidate(){
+        bulletLifeTime = range / bulletSpeed;
+        timeBetweenShots = 1 / fireRate;
+
+    }
+
+    
+}
