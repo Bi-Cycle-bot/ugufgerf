686c47181140191312d2a2fb5f13b0592eda44d9
Shadragon99 <haaris2001@gmail.com>
2023-05-08 14:19:44 -0700

    Audio Update

---
+++ Splattered/Assets/Scripts/Haaris-Scripts/FadeManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeManager : MonoBehaviour
+{
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.volume = 0f;
+        StartCoroutine(Fade(true, source, 5f, 0.175f));
+        StartCoroutine(Fade(false, source, 3f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Fade(bool fadeIn, AudioSource source, float duration, float targetVolume)
+    {
+        if(!fadeIn)
+        {
+            double lengthOfSource = (double) source.clip.samples / source.clip.frequency;
+            yield return new WaitForSecondsRealtime((float)(lengthOfSource - duration));
+        }
+
+        float time =0f;
+        float startVol = source.volume;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            source.volume = Mathf.Lerp(startVol, targetVolume, time/duration);
+            yield return null;
+        }
+
+    } 
+}
