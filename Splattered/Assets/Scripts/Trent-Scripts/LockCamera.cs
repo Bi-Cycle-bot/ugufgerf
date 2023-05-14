91e3ea903e051a1840058ac88b7861aa617db499
TrentLucas <itsstatiic@gmail.com>
2023-05-13 19:21:15 -0700

    Third Level Add-ons

--- Splattered/Assets/Scripts/Trent-Scripts/LockCamera.cs
+++ Splattered/Assets/Scripts/Trent-Scripts/LockCamera.cs
@@ -22,31 +22,61 @@ public class LockCamera : MonoBehaviour
 
     void Update()
     {
-    
-        // floor 1 checker
-        if (player.transform.position.y < 7) {
-            camManager.minLimits = oldMinLimits;
-            camManager.maxLimits = oldMaxLimits;
-        }
 
         //floor 2 checker
-        if (player.transform.position.y >= 7 && player.transform.position.y < 16) {
+        if ((player.transform.position.y >= 7 && player.transform.position.y < 13) && player.transform.position.x <= 51) {
             Vector2 floor2 = new Vector2(1, 10);
             camManager.minLimits = floor2;
         }
 
         //floor 3 checker
-        if (player.transform.position.y >= 16) {
+        else if (player.transform.position.y >= 16 && player.transform.position.x <= 55) {
             Vector2 floor3 = new Vector2(1, 19);
             camManager.minLimits = floor3;
         }
 
         // vertical room checker
-        if (player.transform.position.x > 56) {
+        else if ((player.transform.position.x >= 56 && player.transform.position.x <= 72) && player.transform.position.y >= 16) {
             camManager.minLimits = newMinLimits;
             camManager.maxLimits = newMaxLimits;
             camManager.mouseFactor = 0.0f;
             camManager.mouseLimit = 0.0f;
+        }
+
+        // vent checker
+        else if ((player.transform.position.x >= 82 && player.transform.position.x <= 101) && (player.transform.position.y >= 22 && player.transform.position.y <= 23)) {
+            cam.orthographicSize = 2.0f;
+        }
+        else if ((player.transform.position.x >= 82 && player.transform.position.x <= 83) && (player.transform.position.y >= 18 && player.transform.position.y <= 23)) {
+            cam.orthographicSize = 2.0f;
+        }
+        else if ((player.transform.position.x >= 82 && player.transform.position.x <= 90) && (player.transform.position.y >= 18 && player.transform.position.y <= 19)) {
+            cam.orthographicSize = 2.0f;
+        }
+        else if ((player.transform.position.x >= 89 && player.transform.position.x <= 90) && (player.transform.position.y >= 15 && player.transform.position.y <= 19)) {
+            cam.orthographicSize = 2.0f;
+        }
+        else if ((player.transform.position.x >= 87 && player.transform.position.x <= 90) && (player.transform.position.y >= 15 && player.transform.position.y <= 16)) {
+            cam.orthographicSize = 2.0f;
+        }
+        else if ((player.transform.position.x >= 87 && player.transform.position.x <= 88) && (player.transform.position.y >= 15 && player.transform.position.y <= 17)) {
+            cam.orthographicSize = 2.0f;
+        }
+        else if ((player.transform.position.x >= 75 && player.transform.position.x <= 88) && (player.transform.position.y >= 16 && player.transform.position.y <= 17)) {
+            cam.orthographicSize = 2.0f;
+        }
+        else if ((player.transform.position.x >= 75 && player.transform.position.x <= 76) && (player.transform.position.y >= 13 && player.transform.position.y <= 17)) {
+            cam.orthographicSize = 2.0f;
+        }
+        else if ((player.transform.position.x >= 50 && player.transform.position.x <= 76) && (player.transform.position.y >= 13 && player.transform.position.y <= 14)) {
+            cam.orthographicSize = 2.0f;
+        }
+
+        // set default settings/ floor 1 checker
+        else {
+            camManager.minLimits = oldMinLimits;
+            camManager.maxLimits = oldMaxLimits;
+            cam.orthographicSize = 5.0f;
         }
     }
 }
