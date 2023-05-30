d5ef711cea37912f2d09af1f706ddf975b769be7
Shadragon99 <haaris2001@gmail.com>
2023-05-29 20:54:03 -0700

    Update on damagePopUp and Tutorial Level

--- Splattered/Assets/Scripts/Haaris-Scripts/DamagePopUp.cs
+++ Splattered/Assets/Scripts/Haaris-Scripts/DamagePopUp.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DamagePopUp : MonoBehaviour
 {
+
+    public static DamagePopUp Create(Vector3 position, int damageAmount) {
+
+        Transform damagePopUpTransform = Instantiate(GameAsset.i.dPopUp, position, Quaternion.identity);
+        
+        DamagePopUp popUp = damagePopUpTransform.GetComponent<DamagePopUp>();
+        popUp.SetUp(damageAmount);
+
+        return popUp;
+    }
+    private TextMeshPro textMesh;
+
+    public void Awake(){
+        textMesh = transform.GetComponent<TextMeshPro>();
+    }
     
+    public void SetUp(int damageAmount){
+        textMesh.SetText(damageAmount.ToString());
+    }
 }
