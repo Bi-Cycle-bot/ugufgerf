6ce11bbe724adbccfc5bfa3f9628759a718ddcff
KingPabloIV <40045413+KingPabloIV@users.noreply.github.com>
2023-05-29 20:31:42 -0700

    enemy respawn

--- Splattered/Assets/Scripts/Trent-Scripts/SpawnPoint.cs
+++ Splattered/Assets/Scripts/Trent-Scripts/SpawnPoint.cs
@@ -6,15 +6,20 @@ public class SpawnPoint : MonoBehaviour
 {
     
     public GameObject player;
+    public GameObject enemies;
+    private GameObject currentEnemies;
     public Transform[] spawnpoint = new Transform[0];
     [HideInInspector]
     public int checkpoint;
 
     void Start() {
         checkpoint = 0;
+        enemies = Instantiate(enemies, Vector3.one, Quaternion.identity);
     }
 
     public void respawnAtCheckpoint() {
         player.transform.position = spawnpoint[checkpoint].position;
+        Destroy(currentEnemies);
+        enemies = Instantiate(enemies, Vector3.one, Quaternion.identity);
     }
 }
