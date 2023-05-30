423ad0c9a2458ac93dbe8a81e4e0ae06cb7fca53
TrentLucas <itsstatiic@gmail.com>
2023-05-30 00:19:42 -0700

    Bug Fixes

--- Splattered/Assets/Scripts/Trent-Scripts/SpawnBoss.cs
+++ Splattered/Assets/Scripts/Trent-Scripts/SpawnBoss.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SpawnBoss : MonoBehaviour
 {
@@ -11,21 +12,41 @@ public class SpawnBoss : MonoBehaviour
     public Vector2 minXY;
     public Vector2 maxXY;
     private bool spawned;
-    private RabbitBoss bossCopy;
+    private RabbitBoss[] bossCopy;
     public GameObject door;
+    public int bossCount;
+    private bool[] bossCopyBool;
+    public GameObject[] spawnpoints;
 
     void Start() {
         spawned = false;
+        bossCopy = new RabbitBoss[bossCount];
+        bossCopyBool = new bool[bossCount];
     }
 
     void Update()
     {
         if ((player.transform.position.x > minXY[0] && player.transform.position.x < maxXY[0]) && (player.transform.position.y > minXY[1] && player.transform.position.y < maxXY[1]) && spawned == false) {
-            bossCopy = Instantiate(boss, spawnPoint.transform.position, Quaternion.identity);
+            for (int i = 0; i < bossCopy.Length; i++) {
+                bossCopy[i] = Instantiate(boss, spawnpoints[i].transform.position, Quaternion.identity);
+            }
             spawned = true;
         }
-        if (bossCopy != null && bossCopy.currentHealth < 0) {
+
+        for (int i = 0; i < bossCopy.Length; i++) {
+            if (bossCopy[i] != null && bossCopy[i].currentHealth < 0) {
+                bossCopyBool[i] = true;
+            }
+        }
+
+        if (Array.TrueForAll(bossCopyBool, trueCondition)) {
             door.SetActive(false);
         }
+
+
+    }
+
+    bool trueCondition(bool value) {
+        return value == true;
     }
 }
