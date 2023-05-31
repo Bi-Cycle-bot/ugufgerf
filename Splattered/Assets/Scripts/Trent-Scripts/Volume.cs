d61b58d475a651f6b2c27b5cdd46c1e0424b74a3
KingPabloIV <40045413+KingPabloIV@users.noreply.github.com>
2023-05-31 01:46:40 -0700

    Revert "Dwdawdwa"
    This reverts commit 923ea333ab41245591a4963f5b854dabca403ef5.

--- Splattered/Assets/Scripts/Trent-Scripts/Volume.cs
+++ Splattered/Assets/Scripts/Trent-Scripts/Volume.cs
@@ -7,19 +7,9 @@ public class Volume : MonoBehaviour
 {
     
     public AudioMixer mixer;
-    private float valueRef;
-
-    void Start() {
-        valueRef = 1.0f;
-    }
 
     public void SetLevel(float value) {
         mixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
-        valueRef = value;
     }
 
-    void OnDisable() {
-        PlayerPrefs.SetFloat("volume", valueRef);
-        Debug.Log(valueRef);
-    }
 }
